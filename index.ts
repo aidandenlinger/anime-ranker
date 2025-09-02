9379cca741abf51683392f188ca5f0e8eb32a7e2
Aidan Denlinger <47428697+aidandenlinger@users.noreply.github.com>
2025-09-02 16:29:30 -0700

    feat: basic anilist req

--- index.ts
+++ index.ts
@@ -1,1 +1,49 @@
-console.log("hello world!");
+interface SearchResp {
+  data: {
+    Media: {
+      averageScore: number;
+      title: {
+        english: string;
+      };
+    };
+  };
+}
+
+/**
+ * Given an anime title, return its average score on anilist.
+ * @param title The title of the anime
+ * @returns The average score, and the title on AniList
+ */
+async function getRanking(
+  title: string,
+): Promise<{ title: string; score: number }> {
+  const query = `query ExampleQuery($search: String) {
+    Media(search: $search) {
+      averageScore
+      title {
+        english
+      }
+    }
+  }
+  `;
+
+  const data = (await (
+    await fetch("https://graphql.anilist.co", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        query,
+        variables: { search: title },
+      }),
+    })
+  ).json()) as SearchResp;
+
+  return {
+    title: data.data.Media.title.english,
+    score: data.data.Media.averageScore,
+  };
+}
+
+console.log(await getRanking("Frieren"));
