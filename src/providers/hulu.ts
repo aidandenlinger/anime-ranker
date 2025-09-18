4d1b31b61b4646aca75f2f0e15e45a334118518b
Aidan Denlinger <47428697+aidandenlinger@users.noreply.github.com>
2025-09-17 23:47:05 -0700

    refactor(hulu): similarly refactor hulu to centralize parsing in zod

--- src/providers/hulu.ts
+++ src/providers/hulu.ts
@@ -1,4 +1,4 @@
-import type { Provider, Video } from "./provider.ts";
+import { type Provider, type Video } from "./provider.ts";
 import { fromURL } from "cheerio";
 import z from "zod";
 
@@ -8,13 +8,18 @@ import z from "zod";
 export class Hulu implements Provider {
   name: Provider["name"] = "Hulu";
 
+  // Of note - they actually have separate pages for TV and movies.
+  // - https://www.hulu.com/sitemap/genres/anime-movies
+  // - https://www.hulu.com/sitemap/genres/anime-tv
+  // I'm sticking with the simple one-request-gets-both approach, but it's
+  // good to have the option if I decide to refactor
   api = new URL("https://www.hulu.com/sitemap/genres/anime");
 
   /**
    * @returns a list of all anime on Hulu.
-   * @throws {Error} if HTML request fails
+   * @throws {Error} if HTML request fails or response isn't as expected
    */
-  async getAnime(): Promise<Video[]> {
+  async getAnime() {
     let $;
     try {
       $ = await fromURL(this.api, {
@@ -33,43 +38,67 @@ export class Hulu implements Provider {
       .map((_, element) => {
         const selector = $(element).children().first();
 
-        const parsed = z
-          .object({
-            provider_title: z.string(),
-            provider_url: z.codec(z.string(), z.instanceof(URL), {
-              decode: (href) => new URL(href, "https://hulu.com"),
-              encode: (value) => value.pathname,
-            }),
-          })
-          .readonly()
-          .parse({
-            provider_title: selector.attr("title"),
-            provider_url: selector.attr("href"),
-          });
-
-        // We don't need the referrer
-        parsed.provider_url.searchParams.delete("lp_referrer");
-
-        let type: Video["type"];
-        if (parsed.provider_url.pathname.startsWith("/movie")) {
-          type = "MOVIE";
-        } else if (parsed.provider_url.pathname.startsWith("/series")) {
-          type = "TV";
-        } else {
-          console.warn(
-            `Unexpected url path ${parsed.provider_url.pathname} doesn't start with '/movie' or '/series'`,
-          );
-          return;
-        }
-
-        return {
-          ...parsed,
-          type,
-          provider: this.name,
-        } satisfies Video;
+        return this.#HuluParsed.parse({
+          title: selector.attr("title"),
+          href: selector.attr("href"),
+        });
       })
       .toArray();
 
     return titles;
   }
+
+  /**
+   * A parsed version of Hulu's site data.
+   */
+  #HuluParsed = z
+    .object({
+      title: z.string(),
+      href: validHuluHref,
+    })
+    .transform(({ title, href }) => {
+      let type: Video["type"];
+      if (hrefIs("/movie", href)) {
+        type = "MOVIE";
+      } else if (hrefIs("/series", href)) {
+        type = "TV";
+      } else {
+        // If I ever add another validHuluUrl type, this will fail compilation :)
+        type = href satisfies never;
+      }
+
+      const provider_url = new URL(href, "https://hulu.com");
+      // We don't need the referrer
+      provider_url.searchParams.delete("lp_referrer");
+
+      return { provider_title: title, provider_url, type, provider: this.name };
+    })
+    .readonly();
+}
+
+// Some URL utilities. I need to define types outside of the class, so it felt
+// better to store everything outside and together.
+
+/** The valid possibilities for a url to start with, relative to hulu.com. */
+const validHrefPrefix = z.enum(["/movie", "/series"]);
+/** The valid possibilities for a url to start with, relative to hulu.com. */
+type ValidHrefPrefix = z.infer<typeof validHrefPrefix>;
+
+/** An acceptable href for a Hulu entry. */
+const validHuluHref = z
+  .templateLiteral([validHrefPrefix, z.string()])
+  .readonly();
+
+/**
+ * A smarter version of `href.startsWith` which tightens the string definition,
+ * allowing for exhaustive checking.
+ * @param prefix The prefix to check for
+ * @param href The href to check the prefix on
+ * @returns if href starts with prefix, with a type assertion
+ */
+function hrefIs<Prefix extends ValidHrefPrefix>(
+  prefix: Prefix,
+  href: string,
+): href is `${Prefix}${string}` {
+  return href.startsWith(prefix);
 }
